52c90b2a67720c424f87f4a726fbabd2b55d9bc0
KHK <ston0538@gmail.com>
2020-09-24 01:38:30 +0900

    input form submit

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,4 +1,7 @@
 describe("Input form", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
   it("focuses input on load", () => {
     cy.focused().should("have.class", "new-todo");
   });
@@ -11,5 +14,17 @@ describe("Input form", () => {
     const typedText = "Buy Milk";
 
     cy.get(".new-todo").type(typedText).should("have.value", typedText);
+  });
+
+  context("Form submission", () => {
+    it.only("Adds a new todo on submit", () => {
+      cy.server();
+      cy.route("POST", "api/todos", {
+        name: "Buy eggs",
+        id: 1,
+        isComplete: false,
+      });
+      cy.get(".new-todo").type("Buy eggs").type("{enter}");
+    });
   });
 });
